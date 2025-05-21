a5c6e6ab8d87daaba8d1aa0a4ebc9803b994ed55
Bagas <bagashyt@gmail.com>
2025-05-21 15:58:11 +0700

    refactor test

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -6,11 +6,12 @@ import {FundMe} from "../src/FundMe.sol";
 import {Script} from "forge-std/Script.sol";
 
 contract DeployFundMe is Script {
-
-    function run() external {
+    
+    function run() external returns (FundMe){
         vm.startBroadcast();
-        new FundMe();
+        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
+        return fundMe;
     }
     
 }
